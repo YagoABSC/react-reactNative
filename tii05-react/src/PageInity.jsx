87f26a63fc28762edebb705310f26a7e0189f7c5
YagoSinu <yagoabscosta@gmail.com>
2024-10-25 22:02:05 -0300

    um bom começo

---
+++ tii05-react/src/PageInity.jsx
@@ -0,0 +1,10 @@
+function PageInity() {
+    return(
+        <>
+            <h2 >Welcome to React</h2>
+            <p>This is my first code in REACTjs</p>
+        </>
+    )
+}
+
+export default PageInity;
