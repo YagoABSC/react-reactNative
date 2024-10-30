71b651077ab52ff1d0233a7c031cb42828c292c9
YagoSinu <yagoabscosta@gmail.com>
2024-10-30 19:56:12 -0300

    avançando mais um pouco

--- tarefador/src/ToDoList.jsx
+++ tarefador/src/ToDoList.jsx
@@ -1,24 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import './ToDoList.css';
 
 function ToDoList() {
+
+    const [lista, setLista] = useState([]);
+    const [novoItem, setNovoItem] = useState("");
+
+    function addItem(form) {
+        form.preventDefault();
+        if (!novoItem) {
+            return;
+        }
+        setLista([...lista, { text: novoItem, isCompleted: false }]);
+        setNovoItem("");
+        document.getElementById('input-entrada').focus();
+    }
+
     return (
         <>
             <h1>Meu tarefador</h1>
-            <form action="">
-                <input type="text" placeholder="O que tu quer fazer?" />
+            <form onSubmit={addItem}>
+                <input
+                    id="input-entrada"
+                    type="text" value={novoItem} onChange={(e) => { setNovoItem(e.target.value) }} placeholder="O que tu quer fazer?" />
                 <button className="add">add</button>
             </form>
             <div className="listaTarefas">
-                <div className="item">
-                    <span>Faça isso</span>
-                    <button>Deletar</button>
+                <div>
+                    {
+                        lista.length < 1
+                            ?
+                            "Sem tarefas pendentes"
+                            :
+
+                            lista.map((item, index) => (
+                                <div className="item">
+                                    <span>{item.text}</span>
+                                    <button className="del">Deletar</button>
+                                </div>
+                            ))
+
+                    }
                 </div>
+
                 <div className="item completo">
                     <span>Faça isso</span>
-                    <button>Deletar</button>
+                    <button className="del">Deletar</button>
                 </div>
             </div>
+            <button className="delAll">Apagar tudo</button>
         </>
     )
 }
