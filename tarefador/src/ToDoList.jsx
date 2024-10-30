edfddbec8a84e69b78692afbe3d4b81aa386df2f
Yago <142351190+YagoABSC@users.noreply.github.com>
2024-10-30 19:05:16 -0300

    avançando

--- tarefador/src/ToDoList.jsx
+++ tarefador/src/ToDoList.jsx
@@ -4,7 +4,21 @@ import './ToDoList.css';
 function ToDoList() {
     return (
         <>
-            <h1>Olá mundo</h1>
+            <h1>Meu tarefador</h1>
+            <form action="">
+                <input type="text" placeholder="O que tu quer fazer?" />
+                <button className="add">add</button>
+            </form>
+            <div className="listaTarefas">
+                <div className="item">
+                    <span>Faça isso</span>
+                    <button>Deletar</button>
+                </div>
+                <div className="item completo">
+                    <span>Faça isso</span>
+                    <button>Deletar</button>
+                </div>
+            </div>
         </>
     )
 }
